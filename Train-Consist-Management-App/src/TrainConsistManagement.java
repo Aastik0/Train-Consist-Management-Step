f83982d4ba78850eb96768e602d0440a3601b946
Aastik0 <aastik7898@gmail.com>
2026-04-07 08:45:35 +0530

    UC17: Sort bogie names using Arrays.sort()

--- Train-Consist-Management-App/src/TrainConsistManagement.java
+++ Train-Consist-Management-App/src/TrainConsistManagement.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 /**
  * Train Consist Management Application
  *
- * UC16: Bubble Sort for Passenger Bogie Capacities
+ * UC17: Sorting using Arrays.sort()
  *
  * @author Aastik
- * @version 16.0
+ * @version 17.0
  */
 
 public class TrainConsistApp {
@@ -16,32 +18,16 @@ public class TrainConsistApp {
         System.out.println(" Train Consist Management App ");
         System.out.println("====================================");
 
-        // ===== UC16 =====
-        int[] capacities = {72, 56, 24, 70, 60};
+        // ===== UC17 =====
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
         System.out.println("\nBefore Sorting:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println(Arrays.toString(bogieNames));
 
-        // 🔥 Bubble Sort
-        int n = capacities.length;
+        // 🔥 Built-in sorting
+        Arrays.sort(bogieNames);
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-
-                if (capacities[j] > capacities[j + 1]) {
-                    // Swap
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-
-        System.out.println("\n\nAfter Sorting:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("\nAfter Sorting:");
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
